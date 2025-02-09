bcbb1986a64cbbcd3b726a8322beef7ee0870568
Shreya Mohod <mohodshreya>
2025-02-09 11:22:09 +0530

    hopefully finalll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,29 +5,55 @@ import { FiMenu, FiX } from "react-icons/fi"; // Import icons
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  // Function to handle link click (scroll to top + close mobile menu)
+  const handleLinkClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setMenuOpen(false); // Close mobile menu if open
+  };
+
   return (
     <nav className="fixed w-full z-50">
       <div className="container mx-auto px-6 font-bold py-6 flex items-center justify-end">
         {/* Logo */}
-        <Link to="/" className="absolute left-6 flex items-center">
+        <Link
+          to="/"
+          className="absolute left-6 flex items-center"
+          onClick={handleLinkClick}
+        >
           <img src="/logo.png" alt="Monolith Academy Logo" className="h-12" />
         </Link>
 
         {/* Desktop Menu */}
         <div className="hidden md:flex space-x-6 ml-auto">
-          <Link to="/" className="text-gray-800 relative group">
+          <Link
+            to="/"
+            className="text-gray-800 relative group"
+            onClick={handleLinkClick}
+          >
             Home
             <span className="absolute bottom-[-6px] left-0 w-0 h-[3px] bg-[#f5979a] group-hover:w-full transition-all duration-300"></span>
           </Link>
-          <Link to="/about" className="text-gray-800 relative group">
+          <Link
+            to="/about"
+            className="text-gray-800 relative group"
+            onClick={handleLinkClick}
+          >
             About
             <span className="absolute bottom-[-6px] left-0 w-0 h-[3px] bg-[#f5979a] group-hover:w-full transition-all duration-300"></span>
           </Link>
-          <Link to="/courses" className="text-gray-800 relative group">
+          <Link
+            to="/courses"
+            className="text-gray-800 relative group"
+            onClick={handleLinkClick}
+          >
             Courses
             <span className="absolute bottom-[-6px] left-0 w-0 h-[3px] bg-[#f5979a] group-hover:w-full transition-all duration-300"></span>
           </Link>
-          <Link to="/contact" className="text-gray-800 relative group">
+          <Link
+            to="/contact"
+            className="text-gray-800 relative group"
+            onClick={handleLinkClick}
+          >
             Contact
             <span className="absolute bottom-[-6px] left-0 w-0 h-[3px] bg-[#f5979a] group-hover:w-full transition-all duration-300"></span>
           </Link>
@@ -49,7 +75,7 @@ const Navbar = () => {
               <Link
                 to="/"
                 className="block py-2 text-gray-800 hover:text-blue-600"
-                onClick={() => setMenuOpen(false)}
+                onClick={handleLinkClick}
               >
                 Home
               </Link>
@@ -58,7 +84,7 @@ const Navbar = () => {
               <Link
                 to="/about"
                 className="block py-2 text-gray-800 hover:text-blue-600"
-                onClick={() => setMenuOpen(false)}
+                onClick={handleLinkClick}
               >
                 About
               </Link>
@@ -67,7 +93,7 @@ const Navbar = () => {
               <Link
                 to="/courses"
                 className="block py-2 text-gray-800 hover:text-blue-600"
-                onClick={() => setMenuOpen(false)}
+                onClick={handleLinkClick}
               >
                 Courses
               </Link>
@@ -76,7 +102,7 @@ const Navbar = () => {
               <Link
                 to="/contact"
                 className="block py-2 text-gray-800 hover:text-blue-600"
-                onClick={() => setMenuOpen(false)}
+                onClick={handleLinkClick}
               >
                 Contact
               </Link>
